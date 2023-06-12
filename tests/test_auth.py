708bb6aa0e664bd090bacd82fef3d21c081e1069
peresypkinmi <peresypkin.ma@sibvaleo.com>
2023-06-13 01:06:07 +0700

    add test ayth

---
+++ tests/test_auth.py
@@ -0,0 +1,12 @@
+
+
+class TestAuth:
+
+    def test_auth_by_contract(self):
+        pass
+
+    def test_auth_by_phone(self):
+        pass
+
+    def test_auth_with_wrong_password(self):
+        pass
