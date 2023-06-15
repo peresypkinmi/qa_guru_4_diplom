c61df734edd13ca9151bf6109981002756eb9172
peresypkinmi <peresypkin.ma@sibvaleo.com>
2023-06-15 10:04:20 +0700

    added allure notifications

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,7 +3,10 @@
 class TestAuth:
 
     def test_auth_by_contract(self):
-        pass
+        for i in range(10000):
+            s = i
+        assert s == 9999
+
 
     def test_auth_by_phone(self):
         pass
